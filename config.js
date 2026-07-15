c80f1617632b0c1a0c70ea35143cf11e35a2955a
uziuzborn <m.a.o.umarov@gmail.com>
2026-07-15 17:21:46 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.IVIDEON_SEABATTLE_CONFIG = Object.freeze({
   supabaseUrl: 'https://teyilcysjsvitpkwyxom.supabase.co',
-  supabasePublishableKey: 'sb_publishable_prgVbagdOWt0rDyH1IW8lw_GIUeEqcA',
-  rowId: 'main'
+  supabasePublishableKey: 'sb_publishable_prgVbagdOWt0rDyH1IW8lw_GIUeEqcA'
 });
